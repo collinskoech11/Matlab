d8975dc31c7ffda284627341be60c05c027b079d
collinskoech11 <collinskoechck34@gmail.com>
2021-04-09 12:24:26 +0300

    update

--- a10q1.m
+++ a10q1.m
@@ -1,10 +1,18 @@
-import random as r
-secret = r . randint (1 , 10)
-guess = int ( input ( " Guess a number from 1 to 10! " ))
-while guess != secret :
-if guess < secret :
-print ( " Try higher ! " )
-else :
-print ( " Try lower ! " )
-guess = int ( input ( " Guess again : " ))
-print ( " You got it , the secret number was " , secret )
+secret = randi([1,10]);
+numb = 0;
+n = 0;
+Numb=[];
+while numb ~= r
+    numb = input('Guess a number from 1 to 10: ');
+    if ~ismember(numb,Numb)
+        n = n+1;
+        Numb=[Numb,numb];
+    end
+    if numb < secret
+        fprintf('Try higher', numb)
+    elseif numb > r
+        fprintf('Try lower \n', numb)
+    else
+        fprintf('You got it , the secret number was', secret)
+    end
+end

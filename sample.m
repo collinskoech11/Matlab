8de89b6d1d9dc4997d1e838c88bd9ec00b6f6896
collinskoech11 <collinskoechck34@gmail.com>
2021-04-09 11:07:55 +0300

    update

---
+++ sample.m
@@ -0,0 +1,18 @@
+r = randi([1,10]);
+numb = 0;
+n = 0;
+Numb=[];
+while numb ~= r
+    numb = input('Guess a number from 1 to 10: ');
+    if ~ismember(numb,Numb)
+        n = n+1;
+        Numb=[Numb,numb];
+    end
+    if numb < r
+        fprintf('Try higher', numb)
+    elseif numb > r
+        fprintf('Try lower \n', numb)
+    else
+        fprintf('Congratulations, you guessed the right number %d!\n', r)
+    end
+end
